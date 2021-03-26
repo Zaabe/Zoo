4efed489eb10f96914db3e9fd3a908ffc07ce98a
Zaabe <81359703+Zaabe@users.noreply.github.com>
2021-03-26 15:26:10 +0100

    Add files via upload

--- ZooMain.java
+++ ZooMain.java
@@ -14,15 +14,16 @@ public class ZooMain {
 		
 		Checks game = new Checks();
 		Animals spawnAnim = new Animals();
+		int turn = 0;
 		
 		String animal;
 		
-		while(game.stop() == 0) {
+		while(game.stop(turn) == 0) {
 			// La partita va avanti
 			animal = spawnAnim.spawn();
 			System.out.println("E' apparso un animale!: " + animal);
 			spawnAnim.feed(animal);
-			
+			turn = turn + 1;
 		}
 		
 		System.out.println("Ciao! Torna presto!");
